de13053f800cf1469d9a99d45345863b1e3a48c1
Mezix <mezteenx@googlemail.com>
2020-08-22 01:09:58 +0200

    addded tilesets and some basic animation

---
+++ Joff Studios - The Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float _moveSpeed = 1f;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * _moveSpeed * Time.fixedDeltaTime);
+    }
+}
