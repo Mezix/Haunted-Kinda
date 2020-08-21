5a1105c3d76f604e20acb8a3051cfd2d1ab324be
Mezix <mezteenx@googlemail.com>
2020-08-21 21:04:46 +0200

    added extra packages and tools to the project

---
+++ Joff Studios - The Game/Assets/Scripts/EventSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystem : MonoBehaviour
+{
+    public static EventSystem current;
+    private void Awake()
+    {
+        current = this;
+    }
+
+    //public event Action BattleStartEvent;
+    //public event Action<Unit> UnitChangedEvent;
+}
