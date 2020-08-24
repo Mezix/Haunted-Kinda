8ac0c7b06b109b4e643fde524d07c1a983debf95
Mezix <mezteenx@googlemail.com>
2020-08-24 18:29:33 +0200

    added ui stuff

--- Joff Studios - The Game/Assets/Scripts/LevelScene/UI.cs
+++ Joff Studios - The Game/Assets/Scripts/LevelScene/UI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,11 +15,34 @@ public class UI : MonoBehaviour
     public GameObject PauseScreen;
     public GameObject SunDialObject;
     public GameObject SunDial;
+    public GameObject Buttons;
+    public GameObject buttonWithSprite;
+    public GameObject Instructions;
+
+    public Sprite E;
+    public Sprite Q;
+
 
     private void Update()
     {
         SetSundialRotation();
         SetDashmeterFill();
+
+        SelectButtonToShow();
+    }
+
+    private void SelectButtonToShow()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, 1 << 9);
+        if (hit.collider.TryGetComponent<Offering>(out Offering offering))
+        {
+
+            if (!offering.disappearing)
+            {
+                
+                offering.gameObject.SetActive(false);
+            }
+        }
     }
 
     void SetDashmeterFill()
@@ -37,10 +61,14 @@ public class UI : MonoBehaviour
         //enabled
         portrait.SetActive(true);
         DashMeter.SetActive(true);
+        SunDialObject.SetActive(true);
+        SunDial.SetActive(true);
 
         //disabled
         EndScreen.SetActive(false);
         PauseScreen.SetActive(false);
+        Instructions.SetActive(false);
+        Buttons.SetActive(false);
     }
 
     public void UIDash()
@@ -77,6 +105,18 @@ public class UI : MonoBehaviour
     public void UIUnpause()
     {
         PauseScreen.SetActive(false);
-        //Time.timeScale = 1;
+        Instructions.SetActive(false);
+    }
+
+    public void Help()
+    {
+        if(Instructions.activeSelf)
+        {
+            Instructions.SetActive(false);
+        }
+        else
+        {
+            Instructions.SetActive(true);
+        }
     }
 }
